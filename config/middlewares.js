bd4017cbbc5dde22e0e957d60dc9aaa501c4887a
tokileecy <tokileecy@gmail.com>
2023-03-28 20:37:46 +0800

    fix: image can't preview due to security policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,17 @@
-module.exports = [
+module.exports = ({env}) => ([
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          'img-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
+          'media-src': ["'self'", 'data:', 'blob:', 'storage.googleapis.com'],
+        },
+      }
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +20,4 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]);
